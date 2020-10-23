e77de22bc79355ce17d5990feb554c5cf2562a7e
Sean Kramer <seandominickramer@gmail.com>
2020-10-23 19:35:22 -0400

    Add file structure, server, and logger middleware

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const helmet = require('helmet')
+const logger = require('./middleware/logger')
+// const projectsRouter = require('./routers/projects')
+// const resourcesRouter = require('./routers/resources')
+// const tasksRouter = require('./routers/tasks')
+
+const server = express()
+const port = process.env.PORT || 5000
+
+server.use(helmet())
+server.use(express.json())
+server.use(logger)
+
+server.get('/welcome', (req, res) => {
+  res.send("Welcome to Projects API!")
+})
+
+// server.use(projectsRouter)
+// server.use(resourcesRouter)
+// server.use(tasksRouter)
+
+
+server.listen(port, () => {
+  console.log(`server listening at http://localhost:${port}`)
+})
